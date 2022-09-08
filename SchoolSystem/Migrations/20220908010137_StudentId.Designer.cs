ed877acfea8e2e96c6735ba3eecdb60fcdaa9dc4
stfkolev <inkyzfx@gmail.com>
2022-09-08 04:14:36 +0300

    Added migration for StudentID

--- SchoolSystem/Migrations/20220908010137_StudentId.Designer.cs
+++ SchoolSystem/Migrations/20220908010137_StudentId.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolSystem.Contexts;
 
 namespace SchoolSystem.Migrations
 {
     [DbContext(typeof(MainContext))]
-    partial class MainContextModelSnapshot : ModelSnapshot
+    [Migration("20220908010137_StudentId")]
+    partial class StudentId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SchoolSystem/Migrations/20220908010137_StudentId.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolSystem.Migrations
+{
+    public partial class StudentId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genders",
+                keyColumn: "Id",
+                keyValue: 702600801);
+
+            migrationBuilder.DeleteData(
+                table: "Genders",
+                keyColumn: "Id",
+                keyValue: 1514072092);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "MedicalInformation",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 652768420, "Male" });
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 132381335, "Female" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genders",
+                keyColumn: "Id",
+                keyValue: 132381335);
+
+            migrationBuilder.DeleteData(
+                table: "Genders",
+                keyColumn: "Id",
+                keyValue: 652768420);
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "MedicalInformation");
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 702600801, "Male" });
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1514072092, "Female" });
+        }
+    }
+}
